1eb0e5f6b0201d0a912f6cd8fb2a89487fb628e4
Ivan Developer <Grishchenko.Ivan@gmail.com>
2012-04-24 14:35:46 +0300

    first

---
+++ Cal/src/com/some/drr/CalActivity.java
@@ -0,0 +1,13 @@
+package com.some.drr;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CalActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
